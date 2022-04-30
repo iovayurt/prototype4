edfa0d823bc93840a92ad922463f6550d79d4474
Ilyas Ovayurt <iovayurt@gmail.com>
2022-04-30 23:59:28 +0300

    enenmy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody enemyRb;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyRb.AddForce((player.transform.position - transform.position).normalized * speed);
+    }
+}
